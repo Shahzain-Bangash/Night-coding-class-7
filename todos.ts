033b5bccf5599c528d7692951d0180d7879bff69
Shahzain-Bangash <165349198+Shahzain-Bangash@users.noreply.github.com>
2024-04-06 11:17:10 -0700

    todos_code

---
+++ todos.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let arrayAns: any[] = [];
+let condition = true;
+
+while (condition) {
+  let todsAns = await inquirer.prompt([
+    {
+      name: "Q1",
+      type: "input",
+      message: "what do you like to add in todos? ",
+    },
+    {
+      name: "Q2",
+      type: "confirm",
+      message: "Do you want to add more? ",
+      default: false,
+    },
+  ]);
+
+  condition = todsAns.Q2;
+  arrayAns.push(todsAns.Q1);
+
+  let n = 1;
+  if (todsAns.Q1 != 0) {
+    for (let i in arrayAns) {
+      console.log(n, arrayAns[i]);
+      n++
+    };
+  } else {
+    let empSentence = "Please add something in todos!";
+    console.log(`"${empSentence}"`);
+  }
+}
+
